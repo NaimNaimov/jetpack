71a28cce154ecf2a6deb1108af2505c61af5a55c
John Caruso <johncaruso@gmail.com>
2020-12-18 13:56:47 -0500

    [not verified] refactor custom videopress field in REST api. Updates tests to support it.

--- tests/php/core-api/wpcom-fields/test-attachment-fields-videopress.php
+++ tests/php/core-api/wpcom-fields/test-attachment-fields-videopress.php
@@ -20,11 +20,11 @@ class Test_WPCOM_REST_API_V2_Attachment_VideoPress_Field extends WP_Test_Jetpack
 		$this->assertSame(
 			array(
 				'$schema'     => 'http://json-schema.org/draft-04/schema#',
-				'title'       => 'jetpack_videopress_guid',
-				'type'        => 'string',
+				'title'       => 'jetpack_videopress',
+				'type'        => 'object',
 				'context'     => array( 'view', 'edit' ),
 				'readonly'    => true,
-				'description' => __( 'Unique VideoPress ID', 'jetpack' ),
+				'description' => __( 'VideoPress Data', 'jetpack' ),
 			),
 			$schema
 		);
@@ -35,23 +35,24 @@ class Test_WPCOM_REST_API_V2_Attachment_VideoPress_Field extends WP_Test_Jetpack
 	 */
 	public function test_attachment_fields_videopress_get() {
 		$mock = $this->getMockBuilder( 'WPCOM_REST_API_V2_Attachment_VideoPress_Field' )
-						->setMethods( array( 'get_videopress_guid' ) )
+						->setMethods( array( 'get_videopress_data' ) )
 						->getMock();
 
 		$mock->expects( $this->exactly( 1 ) )
-				->method( 'get_videopress_guid' )
+				->method( 'get_videopress_data' )
 				->will(
-					$this->returnValue( 'mocked_videopress_guid' )
+					$this->returnValue( 'mocked_videopress_data' )
 				);
 
 		$attachment_id = $this->factory->attachment->create_upload_object( dirname( dirname( __DIR__ ) ) . '/jetpack-icon.jpg', 0 );
 		$object        = array(
-			'id' => $attachment_id,
+			'id'     => $attachment_id,
+			'rating' => 'G',
 		);
 		$request       = new WP_REST_Request( 'GET', sprintf( '/wp/v2/media/%d', $attachment_id ) );
-		$guid          = $mock->get( $object, $request );
+		$data          = $mock->get( $object, $request );
 
-		$this->assertSame( 'mocked_videopress_guid', $guid );
+		$this->assertSame( 'mocked_videopress_data', $data );
 	}
 
 	/**
@@ -63,9 +64,12 @@ class Test_WPCOM_REST_API_V2_Attachment_VideoPress_Field extends WP_Test_Jetpack
 		$attachment->post_mime_type = 'non-video/test';
 		$response                   = new stdClass();
 		$response->data             = array(
-			'jetpack_videopress_guid' => 'my-guid',
+			'jetpack_videopress' => array(
+				'guid'   => 'my-guid',
+				'rating' => 'G',
+			),
 		);
 		$response                   = $plugin->remove_field_for_non_videos( $response, $attachment );
-		$this->assertArrayNotHasKey( 'jetpack_videopress_guid', $response->data );
+		$this->assertArrayNotHasKey( 'jetpack_videopress', $response->data );
 	}
 }
